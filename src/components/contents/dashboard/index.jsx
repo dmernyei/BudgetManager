c1adc6e38f9ae622e322e2a102c611499f70c5c5
Dániel Mernyei <Dniel Mernyei>
2017-01-31 16:58:06 +0100

    Added dashboard features.

--- src/components/contents/dashboard/index.jsx
+++ src/components/contents/dashboard/index.jsx
@@ -1,18 +1,54 @@
 import React, { Component } from 'react'
 import { observer } from 'mobx-react'
+import Header from '../../header'
+
 
 @observer(['state'])
 export default class Dashboard extends Component {
     
-    clicked() {
-        this.props.state.dialogState.show(() => console.log("deleted!"), "You will not be able to restore this item.", "Delete", "Cancel")
+    computeAllocatedAmountSum() {
+        var allocatedAmountSum = 0
+        const goals = this.props.state.goalState.goals
+        goals.forEach(goal => allocatedAmountSum += goal.allocatedamount)
+        return allocatedAmountSum
     }
 
+
+    getGoalInfo() {
+        var goalInfo = []
+        const goals = this.props.state.goalState.goals
+        goals.forEach(goal => goalInfo.push(
+            <div key={goal.id} className="goalInfoHolder">
+                <p className="p-goalInfo">{goal.name}: {goal.allocatedamount} Ft</p>
+            </div>
+        ))
+        return goalInfo
+    }
+
+
     render() {
+        const allocatedAmountSum = this.computeAllocatedAmountSum()
+        const liquidBalance = this.props.state.userState.userLiquidBalance
+        const balance = allocatedAmountSum + liquidBalance
+
+        const centerActionData = {
+            wrap: true,
+            GUI: "Dashboard"
+        }
+
         return(
             <div>
-                <p>dashboard</p>
-                liquid balance: {this.props.state.userState.userLiquidBalance}
+                <Header centerActionData={centerActionData} />
+                <div className="dashboardInfoHolder">
+                    <h4>Balance: {balance} Ft</h4>
+                </div>
+                <div className="dashboardInfoHolder">
+                    <h4>Liquid balance: {liquidBalance} Ft</h4>
+                </div>
+                <div className="dashboardInfoHolder">
+                    <h4>Allocated balance: {allocatedAmountSum} Ft</h4>
+                    {this.getGoalInfo()}
+                </div>
             </div>
         )
     }
